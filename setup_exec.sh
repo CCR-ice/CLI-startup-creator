5cfcb1c86da87afe217869028eaf8058270362fe
CCR-ice <64931368+CCR-ice@users.noreply.github.com>
2022-04-16 21:27:49 -0400

    Create setup_exec.sh

---
+++ setup_exec.sh
@@ -0,0 +1,16 @@
+echo "Enter the file:"
+read file
+if [ -d "/etc/start" ]
+then
+	sh -c "mv \"$file\" \"/etc/start/\""
+	sleep 5
+	sh -c "echo \"/etc/start/$file\" >> /etc/bash.bashrc"
+	echo "Done."
+else
+	sh -c "mkdir \"/etc/start/\""
+	sh -c "mv \"$file\" \"/etc/start/\""
+	sleep 5
+	sh -c "echo \"/etc/start/$file\" >> /etc/bash.bashrc"
+	echo "Done."
+fi
+#finish
