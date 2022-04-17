83685d1f612beecb73a8d181f0580d10dbb6c46b
CCR-ice <64931368+CCR-ice@users.noreply.github.com>
2022-04-16 21:03:56 -0400

    Update setup_SH.sh

--- setup_SH.sh
+++ setup_SH.sh
@@ -2,12 +2,12 @@ echo "Enter the file:"
 read file
 if [ -d "/etc/start" ]
 then
+	sh -c "chmod +x \"$file\""
 	sh -c "mv \"$file\" \"/etc/start/\""
-	sh -c "chmod +x $file"
 	sh -c "echo \"./start/$file\" >> /etc/bash.bashrc"
 else
+	sh -c "chmod +x \"$file\""
 	sh -c "mkdir \"/etc/start/\""
 	sh -c "mv \"$file\" \"/etc/start/\""
-	sh -c "chmod +x $file"
 	sh -c "echo \"./start/$file\" >> /etc/bash.bashrc"
 fi
