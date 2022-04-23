a8f56d99a1abf7f367f71089bb4ee1dd5ba5ec4c
CCR-ice <64931368+CCR-ice@users.noreply.github.com>
2022-04-23 19:47:18 -0400

    Update setup_C.sh

--- setup_C.sh
+++ setup_C.sh
@@ -4,13 +4,13 @@ if [ -d "/etc/start" ]
 then
 	sh -c "gcc -o \"/etc/start/$file\" \"$file\""
 	sleep 5
-	sh -c "echo \"/etc/start/$file\" >> /etc/bash.bashrc &&"
+	sh -c "echo \"/etc/start/$file\" >> /etc/bash.bashrc"
 	echo "Done."
 else
 	sh -c "gcc -o \"/etc/start/$file\" \"$file\""
 	sh -c "mkdir \"/etc/start/\""
 	sleep 5
-	sh -c "echo \"/etc/start/$file\" >> /etc/bash.bashrc &&"
+	sh -c "echo \"/etc/start/$file\" >> /etc/bash.bashrc"
 	echo "Done."
 fi
 #finish
